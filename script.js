ac66f6b6ddf491ef24a51a65edef8328aea718d1
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2023-02-26 00:11:18 -0800

    worked on lefr and right movement

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let grid = new Array(20).fill("").map(() => new Array(10).fill(""));
 let canvas;
 let ctx;
 let fallingPiece = [];
+let isGameOver = false;
+let move = "";
+
+let row;
+let col;
+
 window.addEventListener('load', function () {
   canvas = document.getElementById("mainGrid");
   ctx = canvas.getContext("2d");
@@ -11,33 +17,33 @@ window.addEventListener('load', function () {
   mainLoop(ctx);
 })
 
-function changeGrid(row, col, color, ctx){
+function changeGrid(row, col, color, ctx) {
   ctx.fillStyle = color;
 
-  for(let i = 0; i < 29; ++i){
-    ctx.fillRect( i + 30*col, 30*row, 1, 1 );           // draw top border
-    ctx.fillRect( 29 + 30*col, i + 30*row, 1, 1 );      // draw right border
-    ctx.fillRect( 30*col, 29 - i + 30*row, 1, 1 );        // draw bottom border
-    ctx.fillRect( 29 - i + 30*col, 29 + 30*row, 1, 1 );   // draw left border
+  for (let i = 0; i < 29; ++i) {
+    ctx.fillRect(i + 30 * col, 30 * row, 1, 1);           // draw top border
+    ctx.fillRect(29 + 30 * col, i + 30 * row, 1, 1);      // draw right border
+    ctx.fillRect(30 * col, 29 - i + 30 * row, 1, 1);        // draw bottom border
+    ctx.fillRect(29 - i + 30 * col, 29 + 30 * row, 1, 1);   // draw left border
   }
 
   ctx.fillStyle = color;
-  ctx.fillRect(30*col+1, 30*row+1, 28, 28);
+  ctx.fillRect(30 * col + 1, 30 * row + 1, 28, 28);
 }
 
-function delay(ms){
-  return new Promise((resolve) => 
+function delay(ms) {
+  return new Promise((resolve) =>
     setTimeout(resolve, ms)
   );
 }
 
-async function updateDisplay(ctx){
-  return delay(500).then(e => {
-    for(let i = 0; i < 20; ++i){
-      for(let j = 0; j < 10; ++j){
-        if(grid[i][j] !== ""){
+async function updateDisplay(ctx) {
+  return delay(100).then(e => {
+    for (let i = 0; i < 20; ++i) {
+      for (let j = 0; j < 10; ++j) {
+        if (grid[i][j] !== "") {
           changeGrid(i, j, "grey", ctx);
-        }else{
+        } else {
           changeGrid(i, j, "black", ctx);
         }
       }
@@ -45,47 +51,96 @@ async function updateDisplay(ctx){
   })
 };
 
-async function mainLoop(ctx){
-  fallingPiece = [[0,0],[0,1],[0,2],[0,3]];
-  grid[0][0] = "y";
-  grid[0][1] = "y";
-  grid[0][2] = "y";
-  grid[0][3] = "y";
-  let isFalling = true;
-  console.log(fallingPiece)
-  while(isFalling){
-    if(fallingPiece === undefined || fallingPiece.length === 0){
-      break;
-    }
-    let tempFallingPiece = [];
-    for(let [row, col] of fallingPiece){
-      console.log(row)
-      if(row < 19){
-        grid[row+1][col] = grid[row][col];
-        grid[row][col] = "";
-        tempFallingPiece.push([row+1, col])
-      }else{
-        isFalling = false;
-        break;
+async function mainLoop(ctx) {
+  while (!isGameOver) {
+    if (fallingPiece === undefined || fallingPiece.length === 0) {
+      if(grid[0][0] != "y"){
+        fallingPiece = [[0, 0]];
+        grid[0][0] = "y";
       }
     }
-    await updateDisplay(ctx);
+
+    let canFall = (fallingPiece) => {
+      for ([row, col] of fallingPiece) {
+        if (row >= 19 || grid[row + 1][col] != "" || grid[row][col] === "") {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    let tempFallingPiece = [];
+    if (canFall(fallingPiece)) {
+      
+    for ([row, col] of fallingPiece) {
+      if (row < 19) {
+        grid[row + 1][col] = grid[row][col];
+        grid[row][col] = "";
+        tempFallingPiece.push([row + 1, col])
+      } 
+    }
+    
+    
+    }
     fallingPiece = tempFallingPiece;
+
+    if (grid[0][0] != "") {
+      isGameOver = true;
+    }
+
+    await updateDisplay(ctx);
   }
+
 }
 
-document.addEventListener('keydown', function(event) {
-  if(event.key === "ArrowDown") {
+document.addEventListener('keydown', async function (event) {
+  if (event.key === "ArrowDown") {
     console.log("ArrowDown");
-  }else if(event.key === "ArrowLeft") {
-    console.log("ArrowLeft");
-  }else if(event.key === "ArrowRight"){
-    for(let i = 0; i < 20; ++i){
-      for(let j = 0; j < 10; ++j){
-
+  } else if (event.key === "ArrowLeft") {
+    let canMoveLeft = (fallingPiece) => {
+      for ([row, col] of fallingPiece) {
+        if (row <= 0 || grid[row][col - 1] != "")
+          return false;
       }
+
+      return true;
     }
-  }else if(event.key === "ArrowUp"){
+
+    let tempFallingPiece = [];
+    if (canMoveLeft(fallingPiece)) {
+      for([row, col] of fallingPiece){
+        grid[row][col-1] = grid[row][col];
+        grid[row][col] = "";
+        tempFallingPiece.push([row,col - 1]);
+      }
+      fallingPiece = tempFallingPiece;
+    }
+    
+  } else if (event.key === "ArrowRight") {
+      let canMoveRight = (fallingPiece) => {
+        if(fallingPiece.length === 0)
+          return false;
+
+        for ([row, col] of fallingPiece) {
+          if (col >= 9 || grid[row][col + 1] != "")
+            return false;
+        }
+
+        return true;
+      }
+    
+    tempFallingPiece = [];
+    if (canMoveRight(fallingPiece)) {
+      for([row, col] of fallingPiece){
+        console.log(row, col);
+        grid[row][col+1] = grid[row][col];
+        grid[row][col] = "";
+        tempFallingPiece.push([row, col+1]);
+      }
+      fallingPiece = tempFallingPiece;
+    }
+  } else if (event.key === "ArrowUp") {
     console.log("ArrowUp");
   }
 });
